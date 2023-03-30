36bab1b67b37191e1a6de1d6f7b4b54e9096ea49
Syed <syehassan@gmail.com>
2023-03-29 22:58:06 -0700

    Project

---
+++ index.js
@@ -0,0 +1,62 @@
+let slideIndex = 0;
+const carouselButtons = document.querySelectorAll('.carousel-btn');
+
+const setHeroImage = (imageIndex) => {
+    const imagePath = `image-${imageIndex}.jpg`;
+    const heroImage = document.getElementById('hero-image');
+    heroImage.style.backgroundImage = `url(${imagePath})`;
+    heroImage.style.transition = "0.5s";
+};
+
+const setHeroIndicator = (index) => {
+    for(let i = 0; i < carouselButtons.length; i++ ) {
+        if (i === index) {
+            carouselButtons[i].querySelector('span').classList.replace('carousel-num','active');        
+            carouselButtons[i].querySelector('img').setAttribute('src', 'sliderIndicator.svg');
+        } else {
+            carouselButtons[i].querySelector('span').classList.replace('active','carousel-num');
+            carouselButtons[i].querySelector('img').setAttribute('src', 'indicatorBubble.svg');
+        }
+    }
+};
+
+const showHeroSlides = () => {
+    if(slideIndex > 2) {
+        slideIndex = 0;
+    }
+    setHeroImage(slideIndex);
+    setHeroIndicator(slideIndex);
+
+    slideIndex++;
+};
+
+let slideshowInterval = setInterval(showHeroSlides, 4000);
+
+carouselButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        clearInterval(slideshowInterval);
+        slideIndex = parseInt(button.getAttribute('data-slide')) - 1;
+        showHeroSlides
+    ();
+        slideshowInterval = setInterval(showHeroSlides
+        , 4000);
+    });
+});
+
+// Accordion 
+let faqs = document.querySelectorAll('.faq');
+let arrow = document.querySelectorAll('.faq-arrow');
+let icons = document.querySelectorAll('.aIcon');
+let originalIcon = 'plus.svg';
+let newIcon = 'minus.svg';
+
+faqs.forEach((faq, index) => {
+    faq.addEventListener("click", () => {
+        faq.classList.toggle('active');
+        icons.forEach((icon, i) => {
+            if (i === index) {
+                icon.setAttribute('src', icon.getAttribute('src') === originalIcon ? newIcon : originalIcon);
+            }
+        });
+    });
+});
